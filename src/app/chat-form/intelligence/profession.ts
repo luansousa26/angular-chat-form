cf8ddcbb87fffd725f8f9b5c106abc4c490a12ef
luansousa26 <luan2850@gmail.com>
2018-09-30 17:22:48 -0300

    Verify name added in profissions

--- src/app/chat-form/intelligence/profession.ts
+++ src/app/chat-form/intelligence/profession.ts
@@ -1,9 +1,14 @@
+import { EqualityVerification } from "src/app/chat-form/intelligence/name-equality-verification";
+
 export class Professions {
-  profession = [
-      {name: 'developer', message:'Are you my dad?'},
-      {name: 'engineer', message:'You make buildings?'},
-      {name: 'helper', message:'Cool profession.'},
-      {name: 'businessman', message:'What is you actuation area?'},
-      {name: 'support analyst', message:'Cool, you like of callings?'},
+  public static professions = [
+    { name: 'developer', message: 'Are you my dad?' },
+    { name: 'engineer', message: 'You make buildings?' },
+    { name: 'helper', message: 'Cool profession.' },
+    { name: 'businessman', message: 'What is you actuation area?' },
+    { name: 'support analyst', message: 'Cool, you like of callings?' },
   ];
+  public static verifyProfession(profession: string): string {
+    return EqualityVerification.verifyNameEquality(this.professions, profession);
+  }
 }
