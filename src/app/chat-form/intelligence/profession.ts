b960a0ccd3f8e271a044d677b15854d1cde2910e
luansousa26 <luan2850@gmail.com>
2018-10-04 17:14:33 -0300

    new profession added

--- src/app/chat-form/intelligence/profession.ts
+++ src/app/chat-form/intelligence/profession.ts
@@ -2,5 +2,6 @@ export class Professions {
   profession = [
       {name: 'developer', message:'Are you my dad?'},
       {name: 'engineer', message:'You make buildings?'},
+      {name: 'helper', message:'Cool profession.'},
   ];
 }
