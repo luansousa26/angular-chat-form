6abd1e8e620ec5cd68148f6bce20b0ce5c08bd00
Luan Sousa <luan26@uninove.edu.br>
2018-10-07 15:41:03 -0300

    Alteration

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -10,7 +10,8 @@ export class ChatFormComponent implements OnInit {
   quiz: any[] = [
     { number: 0, id: 1, question: 'Hello, Whats your name?', type: 'text', reply: 'Hi my name is luan', comments: [] },
     { number: 1, id: 2, question: 'How many years do you have?', type: 'text', reply: '23 years', comments: [] },
-    { number: 2, id: 3, question: 'Where do you from?', type: 'text', reply: null, comments: [] }
+    { number: 2, id: 3, question: 'Where do you from?', type: 'text', reply: 'Alasca', comments: [] },
+    { number: 3, id: 4, question: 'Do you like a sanduiche?', type: 'text', reply: null, comments: [] },
   ];
   constructor() { }
 
