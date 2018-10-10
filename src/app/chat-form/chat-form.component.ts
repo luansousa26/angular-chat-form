6d15e60f12d401e931f7d81a4ce1573d71e871b1
Luan Sousa <luan26@uninove.edu.br>
2018-10-10 14:34:40 -0300

    Alterations in code

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,26 +1,58 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-chat-form',
-  templateUrl: './chat-form.component.html',
-  styleUrls: ['./chat-form.component.scss']
+  selector: "app-chat-form",
+  templateUrl: "./chat-form.component.html",
+  styleUrls: ["./chat-form.component.scss"]
 })
 export class ChatFormComponent implements OnInit {
   field: any;
   quiz: any[] = [
-    { number: 0, id: 1, question: 'Hello, Whats your name?', type: 'text', reply: null, comments: [] },
-    { number: 1, id: 2, question: 'How many years do you have?', type: 'text', reply: null, comments: [] },
-    { number: 2, id: 3, question: 'Where do you from?', type: 'text', reply: null, comments: [] },
-    { number: 3, id: 4, question: 'Do you like a sanduiche?', type: 'number', reply: null, comments: [] },
+    {
+      number: 0,
+      id: 1,
+      question: "Hello, Whats your name?",
+      type: "text",
+      reply: null,
+      comments: []
+    },
+    {
+      number: 1,
+      id: 2,
+      question: "How many years do you have?",
+      type: "number",
+      reply: null,
+      comments: []
+    },
+    {
+      number: 2,
+      id: 3,
+      question: "Where do you from?",
+      type: "text",
+      reply: null,
+      comments: []
+    },
+    {
+      number: 3,
+      id: 4,
+      question: "Do you like a sanduiche?",
+      type: "text",
+      reply: null,
+      comments: []
+    }
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   updateResp(question: any, value: any): void {
     console.log(question);
     console.log(value);
     this.quiz[question.number].reply = value;
+  }
+
+  @HostListener("document:keypress", ["$event"])
+  public handleKeyboardEvent(): boolean {
+    return true;
   }
 }
