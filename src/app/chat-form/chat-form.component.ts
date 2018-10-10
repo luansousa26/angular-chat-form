eaf540582ffa65f229469d5f209df24818532ccc
Luan Sousa <luan26@uninove.edu.br>
2018-10-10 18:06:08 -0300

    alteration in typing

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild, HostListener } from "@angular/core";
-
+import { Component, OnInit, HostListener } from "@angular/core";
 @Component({
   selector: "app-chat-form",
   templateUrl: "./chat-form.component.html",
