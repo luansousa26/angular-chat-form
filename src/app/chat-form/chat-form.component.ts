8d3efe424a6070c3fabd5ee9c833b170edb65c43
Luan Sousa <luan26@uninove.edu.br>
2018-10-10 14:10:03 -0300

    reply working

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class ChatFormComponent implements OnInit {
   field: any;
   quiz: any[] = [
-    { number: 0, id: 1, question: 'Hello, Whats your name?', type: 'text', reply: 'Hi my name is luan', comments: [] },
-    { number: 1, id: 2, question: 'How many years do you have?', type: 'text', reply: '23 years', comments: [] },
-    { number: 2, id: 3, question: 'Where do you from?', type: 'text', reply: 'Alasca', comments: [] },
+    { number: 0, id: 1, question: 'Hello, Whats your name?', type: 'text', reply: null, comments: [] },
+    { number: 1, id: 2, question: 'How many years do you have?', type: 'text', reply: null, comments: [] },
+    { number: 2, id: 3, question: 'Where do you from?', type: 'text', reply: null, comments: [] },
     { number: 3, id: 4, question: 'Do you like a sanduiche?', type: 'number', reply: null, comments: [] },
   ];
   constructor() { }
