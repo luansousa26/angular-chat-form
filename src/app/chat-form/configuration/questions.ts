a6501607a63485d84e5bef7715750179236e5310
Luan Sousa <luan26@uninove.edu.br>
2018-10-16 21:58:30 -0300

    alteration in question class

---
+++ src/app/chat-form/configuration/questions.ts
@@ -0,0 +1,53 @@
+export class Questions {
+    /* File to include all questions */
+    public static questions: any[] = [
+        {
+            number: 0,
+            id: 1,
+            question: 'Hello, Whats your name?',
+            type: 'text',
+            reply: null,
+            comments: 'Cool name guy, do you like Luan Santana?'
+        },
+        {
+            number: 1,
+            id: 2,
+            question: 'How many years do you have?',
+            type: 'number',
+            reply: null,
+            comments: null
+        },
+        {
+            number: 2,
+            id: 3,
+            question: 'Where you from?',
+            type: 'text',
+            reply: null,
+            comments: null
+        },
+        {
+            number: 3,
+            id: 4,
+            question: 'Do you like a sanduiche?',
+            type: 'text',
+            reply: null,
+            comments: null
+        },
+        {
+            number: 4,
+            id: 5,
+            question: 'What is your profession?',
+            type: 'text',
+            reply: null,
+            comments: 'WOW, you must be a nerd!'
+        },
+        {
+            number: 5,
+            id: 6,
+            question: 'What Programming Language do you prefer?',
+            type: 'text',
+            reply: null,
+            comments: 'I preffer BIRL LANGUAGE rsrsrsrsrsrs!'
+        },
+    ];
+}
