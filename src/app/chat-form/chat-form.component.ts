da4683b706d91ffaeeafe691cfc190316e6480ec
Luan Sousa <luan26@uninove.edu.br>
2018-10-16 22:02:05 -0300

    Correction maked in code

--- src/app/chat-form/chat-form.component.ts
+++ src/app/chat-form/chat-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Questions } from './configuration/questions';
 @Component({
   selector: 'app-chat-form',
   templateUrl: './chat-form.component.html',
@@ -6,56 +7,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class ChatFormComponent implements OnInit {
   field: any;
-  quiz: any[] = [
-    {
-      number: 0,
-      id: 1,
-      question: 'Hello, Whats your name?',
-      type: 'text',
-      reply: null,
-      comments: 'Cool name guy, do you like Luan Santana?'
-    },
-    {
-      number: 1,
-      id: 2,
-      question: 'How many years do you have?',
-      type: 'number',
-      reply: null,
-      comments: null
-    },
-    {
-      number: 2,
-      id: 3,
-      question: 'Where you from?',
-      type: 'text',
-      reply: null,
-      comments: null
-    },
-    {
-      number: 3,
-      id: 4,
-      question: 'Do you like a sanduiche?',
-      type: 'text',
-      reply: null,
-      comments: null
-    },
-    {
-      number: 4,
-      id: 5,
-      question: 'What is your profession?',
-      type: 'text',
-      reply: null,
-      comments: 'WOW, you must be a nerd!'
-    },
-    {
-      number: 5,
-      id: 6,
-      question: 'What Programming Language do you prefer?',
-      type: 'text',
-      reply: null,
-      comments: 'I preffer BIRL LANGUAGE rsrsrsrsrsrs!'
-    },
-  ];
+  quiz  = Questions.questions; // All questions
   typingTimer: any; // control the time that user don't type.
   typingAlert = 'User is typing...'; // alert message.
 
