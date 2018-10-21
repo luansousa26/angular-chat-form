007a5f997f842c93bf858b4f49316659e807e31a
Luan Sousa <luan26@uninove.edu.br>
2018-10-21 14:50:55 -0200

    Alteration in message

--- src/app/chat-form/intelligence/age.ts
+++ src/app/chat-form/intelligence/age.ts
@@ -11,8 +11,11 @@ export class Age {
     ];
 
     public static verifyAge(age: number): string {
-        /* Verify if the ages contains 'age' and return to comment */
+        /*
+        Verify if the ages contains 'age' and return to comment the message in array
+        if age dont exist return another message
+        */
         return this.agesArray.find(agesFind => agesFind.ages.includes(age))
-            ? this.agesArray.find(agesFind => agesFind.ages.includes(age)).message : null;
+            ? this.agesArray.find(agesFind => agesFind.ages.includes(age)).message : 'Are you a immortal? Please teach me the secret!!!';
     }
 }
