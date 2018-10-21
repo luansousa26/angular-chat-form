cfec220feefe72e81f007fcc1b1e6bebe5789bc3
Luan Sousa <luan26@uninove.edu.br>
2018-10-21 19:35:43 -0200

    Countries added

--- src/app/chat-form/intelligence/countries.ts
+++ src/app/chat-form/intelligence/countries.ts
@@ -1,3 +1,8 @@
 export class Countries {
-
+    countries = [
+        { name: 'brazil', message: 'I love Brazil, its my origin country.' },
+        { name: 'paraguay', message: 'Paraguay is a good place to buy things :D.' },
+        { name: 'argentina', message: 'I want meet Buenos Aires.' },
+        { name: 'colombia', message: 'Colombia its a good place.' },
+    ];
 }
