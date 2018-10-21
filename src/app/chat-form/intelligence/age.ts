6b639d3ecbbf7f7076165ed30181fc7e708e9e8c
Luan Sousa <luan26@uninove.edu.br>
2018-10-21 14:13:48 -0200

    class Age created

---
+++ src/app/chat-form/intelligence/age.ts
@@ -0,0 +1,12 @@
+export class Age {
+    age = [
+        {
+            ages: [0, 1, 2, 3, 4],
+            message: 'You really exist? rsrsrs. Or are you a Genius?'
+        },
+        {
+            ages: [5, 6, 7, 8, 9],
+            message: 'You are so young, i hope you like cartoons :D'
+        }
+    ];
+}
