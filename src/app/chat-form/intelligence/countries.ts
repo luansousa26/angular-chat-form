6eddec124c805b892c98fb505652c3488da8ec06
Luan Sousa <luan26@uninove.edu.br>
2018-10-22 16:01:43 -0300

    NameEqualityVerification working well

--- src/app/chat-form/intelligence/countries.ts
+++ src/app/chat-form/intelligence/countries.ts
@@ -1,22 +1,38 @@
-export class Countries {
-    public static countries = [
-        { name: 'brazil', message: 'I love Brazil, its my origin country.' },
-        { name: 'paraguay', message: 'Paraguay is a good place to buy things :D.' },
-        { name: 'argentina', message: 'Quiero conocer argentina Buenos Aires.' },
-        { name: 'colombia', message: 'Colombia its a good place. Some day i will meet Bogotá!' },
-        { name: 'japan', message: 'I LOVE JAPAN, the culture and mangas. mo shindeiru.. naniiii?' },
-        { name: 'china', message: 'Some day i will live in China and learn Shaolin Kung-fu!' },
-        { name: 'north korea', message: 'Pyongyang seems be beautiful.' },
-        { name: 'south korea', message: 'I would like learn k-pop.' },
-        { name: 'spain', message: 'PLUSSSSSSSS ULTRAAAAAAAA!!' },
-        { name: 'united kingdom', message: 'Londres its so perfect, a kingdom sounds good.' },
-        { name: 'narnia', message: 'WOW ASLAN!!' },
-        { name: 'winterfell', message: 'The lone wolf dies but the pack survives!' },
-        { name: 'konoha', message: 'Jiraya for Hokage!' },
-    ];
+import { NameEqualityVerification } from "./name-equality-verification";
 
-    public static verifyCountries(country: string): string {
-        return this.countries.find(cr => country.toLowerCase().includes(cr.name)) ?
-            this.countries.find(cr => country.toLowerCase().includes(cr.name)).message : null;
-    }
+export class Countries {
+  public static countries = [
+    { name: "brazil", message: "I love Brazil, its my origin country." },
+    { name: "paraguay", message: "Paraguay is a good place to buy things :D." },
+    { name: "argentina", message: "Quiero conocer argentina Buenos Aires." },
+    {
+      name: "colombia",
+      message: "Colombia its a good place. Some day i will meet Bogotá!"
+    },
+    {
+      name: "japan",
+      message: "I LOVE JAPAN, the culture and mangas. mo shindeiru.. naniiii?"
+    },
+    {
+      name: "china",
+      message: "Some day i will live in China and learn Shaolin Kung-fu!"
+    },
+    { name: "north korea", message: "Pyongyang seems be beautiful." },
+    { name: "south korea", message: "I would like learn k-pop." },
+    { name: "spain", message: "PLUSSSSSSSS ULTRAAAAAAAA!!" },
+    {
+      name: "united kingdom",
+      message: "Londres its so perfect, a kingdom sounds good."
+    },
+    { name: "narnia", message: "WOW ASLAN!!" },
+    {
+      name: "winterfell",
+      message: "The lone wolf dies but the pack survives!"
+    },
+    { name: "konoha", message: "Jiraya for Hokage!" }
+  ];
+
+  public static verifyCountries(country: string): string {
+    return NameEqualityVerification.verifyNameEquality(this.countries, country);
+  }
 }
