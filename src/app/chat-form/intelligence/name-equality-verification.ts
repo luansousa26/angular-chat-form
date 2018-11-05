d2aee9353145c5ce05495c1157b6e60fa8ad5cba
Luan Sousa <luan26@uninove.edu.br>
2018-11-05 16:31:09 -0200

    Verify name alteration

--- src/app/chat-form/intelligence/name-equality-verification.ts
+++ src/app/chat-form/intelligence/name-equality-verification.ts
@@ -1,7 +1,21 @@
 export class EqualityVerification {
+  /*
+  Verify typed user string include the array string,
+  if not verify if array string include the typed string.
+  */
   public static verifyNameEquality(array: any[], value: any): string {
+    value = value.replace(/\s+/g, "");
     return array.find(cr => value.toLowerCase().includes(cr.name))
       ? array.find(cr => value.toLowerCase().includes(cr.name)).message
+      : this.verifyNameInArray(array, value);
+  }
+
+  /*
+  Verify if the array String includes the typed user string
+  */
+  public static verifyNameInArray(array: any[], value: any): string {
+    return array.find(cr => cr.name.includes(value.toLowerCase()))
+      ? array.find(cr => cr.name.includes(value.toLowerCase())).message
       : null;
   }
 }
