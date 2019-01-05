a7f0727d61a996815451e007d856d8a46215d5eb
Luan Sousa <luan26@uninove.edu.br>
2019-01-05 21:34:29 -0200

    tsLint alteration

--- src/app/chat-form/intelligence/name-equality-verification.ts
+++ src/app/chat-form/intelligence/name-equality-verification.ts
@@ -4,7 +4,7 @@ export class EqualityVerification {
   if not verify if array string include the typed string.
   */
   public static verifyNameEquality(array: any[], value: any): string {
-    value = value.replace(/\s+/g, "");
+    value = value.replace(/\s+/g, '');
     return array.find(cr => value.toLowerCase().includes(cr.name))
       ? array.find(cr => value.toLowerCase().includes(cr.name)).message
       : this.verifyNameInArray(array, value);
